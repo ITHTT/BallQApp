039a9ced38924a1eac7aeef06afb4dec493df01f
ITHTT <hett@tysci.com>
2016-06-07 18:10:16 +0800

    比赛

--- app/src/main/java/com/tysci/ballq/activitys/BallQMatchDetailActivity.java
+++ app/src/main/java/com/tysci/ballq/activitys/BallQMatchDetailActivity.java
@@ -2,17 +2,44 @@ package com.tysci.ballq.activitys;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.tysci.ballq.R;
 import com.tysci.ballq.base.BaseActivity;
+
+import butterknife.Bind;
 
 /**
  * Created by Administrator on 2016/6/7.
  */
 public class BallQMatchDetailActivity extends BaseActivity{
+    @Bind(R.id.iv_home_team_icon)
+    protected ImageView ivHomeTeamIcon;
+    @Bind(R.id.tv_home_team_name)
+    protected TextView tvHomeTeamName;
+    @Bind(R.id.tv_game_time)
+    protected TextView tvGameTime;
+    @Bind(R.id.tv_game_date)
+    protected TextView tvGameDate;
+    @Bind(R.id.iv_away_team_icon)
+    protected ImageView ivAwayTeamIcon;
+    @Bind(R.id.tv_away_team_name)
+    protected TextView tvAwayTeamName;
+    @Bind(R.id.tv_game_league_name)
+    protected TextView tvGameLeagueName;
+
+    @Bind(R.id.tab_layout)
+    protected TabLayout tabLayout;
+    @Bind(R.id.view_pager)
+    protected ViewPager viewPager;
+
     @Override
     protected int getContentViewId() {
-        return 0;
+        return R.layout.activity_ballq_match_detail;
     }
 
     @Override
---
+++ app/src/main/java/com/tysci/ballq/fragments/BallQMatchTipOffListFragment.java
@@ -0,0 +1,7 @@
+package com.tysci.ballq.fragments;
+
+/**
+ * Created by Administrator on 2016/6/7.
+ */
+public class BallQMatchTipOffListFragment {
+}
